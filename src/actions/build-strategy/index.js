44577005121bbe94e67d5fe565dd385352f5fc29
Alessandro Manfredi <alle.manfredi@gmail.com>
2021-01-07 08:49:17 +0100

    feat(actions & settings): starts build and execute logic

--- src/actions/build-strategy/index.js
+++ src/actions/build-strategy/index.js
@@ -1,6 +1,8 @@
 import { OPTION_SELECTED, OPTION_DELETED, OPTIONS_REORDERED, SET_OPTION_INPUTS } from '../../constants'
 import store from '../../store'
 import { v4 as uuidv4 } from 'uuid'
+import DSA from 'dsa-sdk'
+import Web3 from 'web3'
 
 const selectOption = _option => {
   return {
@@ -47,10 +49,78 @@ const reorderOptions = (_startIndex, _endIndex) => {
 }
 
 const buildAndExecute = () => {
-  return {
-    type: 'TODO',
-    payload: {
-      TODO: 'TODO'
+  return async _dispatch => {
+    try {
+      const options = store.getState().buildStrategy.options
+      const provider = store.getState().wallet.provider
+      const smartAccount = store.getState().wallet.smartAccounts
+      const web3 = new Web3(provider)
+      const dsa = new DSA(web3)
+      dsa.setInstance(smartAccount[0].id)
+
+      const withFlashloan = Boolean(
+        options.find(({ method, name }) => name === 'instapool_v2' && method === 'flashBorrowAndCast')
+      )
+
+      const spells = dsa.Spell()
+
+      options.forEach(({ method, name, inputs, args, additionalArgs, argsType }) => {
+        const fixedInputs = Array(args.length + additionalArgs.length).fill('0')
+        for (let i = 0; i < inputs.length; i++) fixedInputs[i] = inputs[i] ? inputs[i] : '0'
+
+        const realArgs = argsType.map((_type, _index) => {
+          switch (_type) {
+            case 'address': {
+              const address = dsa.tokens.info[fixedInputs[_index].toLowerCase()].address
+              if (!address) {
+                throw new Error('Invalid token')
+              }
+              return address
+            }
+            default:
+              return fixedInputs[_index]
+          }
+        })
+
+        spells.add({
+          connector: name,
+          method,
+          args: realArgs
+        })
+      })
+
+      if (withFlashloan) {
+        const flashloanSpell = dsa.Spell()
+        flashloanSpell.add(spells.data[0])
+
+        if (spells.data[0].connector !== 'instapool_v2' && spells.data[0].method !== 'flashBorrowAndCast')
+          throw new Error('Flashloan position is wrong')
+
+        const spellsToEncodeElements = spells.data.slice(1, spells.length)
+        const spellsToEncode = dsa.Spell()
+        spellsToEncodeElements.forEach(_spell => spellsToEncode.add(_spell))
+
+        const calldata = dsa.instapool_v2.encodeFlashCastData(spellsToEncode)
+        flashloanSpell.data[0].args[flashloanSpell.data[0].args.length - 1] = calldata
+
+        await dsa.cast(flashloanSpell)
+
+        return {
+          type: 'TODO',
+          payload: {
+            TODO: 'TODO'
+          }
+        }
+      } else {
+        return {
+          type: 'TODO',
+          payload: {
+            TODO: 'TODO'
+          }
+        }
+      }
+    } catch (_err) {
+      console.error(_err)
     }
   }
 }
--- src/settings/index.js
+++ src/settings/index.js
@@ -10,53 +10,69 @@ const settings =  {
         name: 'aave',
         method: 'deposit',
         args: ['token', 'amount'],
-        id: 6
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'number','number', 'number'],
+        id: 0
       },
       {
         name: 'aave',
         method: 'withdraw',
         args: ['token', 'amount'],
-        id: 7
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'number','number', 'number'],
+        id: 1
       }
     ],
     instapool_v2: [
       {
         name: 'instapool_v2',
         method: 'flashBorrowAndCast',
-        args: ['token', 'amount', 'route'], // NOTE: calldata missing
-        id: 4
+        args: ['token', 'amount', 'protocol'], 
+        additionalArgs: ['calldata'],
+        argsType: ['address', 'number', 'number', 'calldata',],
+        id: 2
       },
       {
         name: 'instapool_v2',
         method: 'flashPayback',
-        args: ['token', 'amount',],
-        id: 5
+        args: ['token', 'amount'],
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'number','number', 'number'],
+        id: 3
       }
     ],
     uniswap: [
       {
         name: 'uniswap',
         method: 'buy',
-        args: ['buyAddr', 'sellAddr', 'buyAmt', 'unitAmt'],
-        id: 0
+        args: ['tokenBuy', 'tokenSell', 'buyAmt', 'unitAmt'],
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'address', 'number', 'number','number', 'number'],
+        id: 4
       },
       {
         name: 'uniswap',
         method: 'sell',
-        args: ['buyAddr', 'sellAddr', 'buyAmt', 'unitAmt'],
-        id: 1
+        args: ['tokenBuy', 'tokenSell', 'buyAmt', 'unitAmt'],
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'address', 'number', 'number','number', 'number'],
+        id: 5
       },
       {
         name: 'uniswap',
         method: 'deposit',
         args: ['tokenA', 'tokenB', 'amtA', 'unitAmt', 'slippage'],
-        id: 2
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'address', 'number', 'number', 'number','number', 'number'],
+        id: 6
       },
       {
         name: 'uniswap',
         method: 'withdraw',
         args: ['tokenA', 'tokenB', 'uniAmt', 'unitAmtA', 'unitAmtB'],
-        id: 3
+        additionalArgs: ['getId', 'setId'],
+        argsType: ['address', 'address', 'number', 'number', 'number','number', 'number'],
+        id: 7
       }
     ],
   }
